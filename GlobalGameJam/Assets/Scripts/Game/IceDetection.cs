e6ad57b4d4699b86db5e5e7a3344c98c761e902e
Voidaerun <francois.petit88@gmail.com>
2019-01-26 13:02:58 -0500

    no message

---
+++ GlobalGameJam/Assets/Scripts/Game/IceDetection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IceDetection : MonoBehaviour
+{
+    public class IceMovementDelegate : IModificator
+    {
+        public void UpdateMove()
+        {
+            Rigidbody.AddForce(new Vector2(Input.Horizontal_L, Input.Vertical_L) * Speed);
+        }
+    }
+
+    public UnityEvent OnIceEnter;
+    public UnityEvent OnIceExit;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.GetComponent<Ice>() != null)
+        {
+            OnIceEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.GetComponent<Ice>() != null)
+        {
+            OnIceExit.Invoke();
+        }
+    }
+
+    private void SetIceDelegate()
+    {
+
+    }
+}
