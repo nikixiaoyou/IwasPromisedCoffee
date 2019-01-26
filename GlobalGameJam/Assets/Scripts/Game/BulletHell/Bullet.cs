6a41155fb1997dcf4a37d5bf9a3897b4dd000300
Frank Brizuela <frank.brizuela@mail.mcgill.ca>
2019-01-26 14:54:30 -0500

    Bullet hell test

--- GlobalGameJam/Assets/Scripts/Game/BulletHell/Bullet.cs
+++ GlobalGameJam/Assets/Scripts/Game/BulletHell/Bullet.cs
@@ -4,9 +4,24 @@ namespace ggj
 {
     public class Bullet : PoolObject
     {
+		public float Speed;
+
+		private const float kWidthBorder = 25f;
+		private const float kHeightBorder = 15f;
+
         public void SetVelocity(Vector2 v)
         {
-            _rigidbody.velocity = v;
+            _rigidbody.velocity = v * Speed;
         }
-    }
+
+		public override void UpdateObject()
+		{
+			base.UpdateObject();
+
+			if (Mathf.Abs(_transform.position.x) > kWidthBorder || Mathf.Abs(_transform.position.y) > kHeightBorder)
+			{
+				Destroy();
+			}
+		}
+	}
 }
---
+++ GlobalGameJam/Assets/Scripts/Game/BulletHell/BulletRestart.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace ggj
+{
+	public class BulletRestart : MonoBehaviour
+	{
+		public Vector2 InitialPosition;
+
+		private Pool pool;
+
+		private void Start()
+		{
+			pool = GameObject.Find("Pool").GetComponent<Pool>();
+
+			RestartLevel();
+		}
+
+		private void OnTriggerEnter2D(Collider2D col)
+		{
+			if (col.tag == "Bullet")
+			{
+				RestartLevel();
+			}
+		}
+
+		private void RestartLevel()
+		{
+			transform.position = InitialPosition;
+
+			if (pool != null)
+			{
+				pool.Reset();
+			}
+		}
+	}
+}
