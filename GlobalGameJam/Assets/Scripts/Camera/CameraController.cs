fc0b732772047a27937a2d3dbd62dce315cab981
Le Hubert <>
2019-01-25 21:58:14 -0500

    setup class layout for player and camera controller

---
+++ GlobalGameJam/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ggj
+{
+    public class CameraController : MonoBehaviour
+    {
+        protected void Awake()
+        {
+            this.Register(this);
+        }
+
+        protected void OnDestroy()
+        {
+            this.UnRegister(this);
+        }
+    }
+}
---
+++ GlobalGameJam/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ggj
+{
+    public class PlayerController : MonoBehaviour
+    {
+        protected void Awake()
+        {
+            this.Register(this);
+        }
+
+        protected void OnDestroy()
+        {
+            this.UnRegister(this);
+        }
+
+
+    }
+}
