e54bc41d6682a1102f5975b0c0d0b8a5c90ece1a
Le Hubert <>
2019-01-25 19:20:24 -0500

    Game manager

---
+++ GlobalGameJam/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ggj
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        public static GameManager Instance { get; private set; }
+
+        public Services Services { get; private set; }
+
+        protected void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            Services = new Services();
+        }
+    }
+}
