5d2ad3aa450a3c1d87878acfebde0b809dab7b08
Frank Brizuela <frank.brizuela@mail.mcgill.ca>
2019-01-26 20:14:30 -0500

    Added HP for bullet hell restart

--- GlobalGameJam/Assets/Scripts/Game/BulletHell/BulletRestart.cs
+++ GlobalGameJam/Assets/Scripts/Game/BulletHell/BulletRestart.cs
@@ -6,12 +6,16 @@ namespace ggj
 	{
 		public Vector2 InitialPosition;
 		public GameObject DeadVFX;
+		public int MaxHp;
 
 		private Pool pool;
+		private PlayerController _playerController;
+		private int _hp;
 
 		private void Start()
 		{
 			pool = GameObject.Find("Pool").GetComponent<Pool>();
+			_playerController = GetComponent<PlayerController>();
 
 			RestartLevel();
 		}
@@ -20,7 +24,13 @@ namespace ggj
 		{
 			if (col.tag == "Bullet")
 			{
-				RestartLevel();
+				_hp--;
+
+				if (_playerController.ShellType != ShellType.rock || _hp <= 0)
+				{
+					RestartLevel();
+					col.GetComponent<Bullet>().Destroy();
+				}
 			}
 		}
 
@@ -33,6 +43,8 @@ namespace ggj
 			{
 				pool.Reset();
 			}
+
+			_hp = MaxHp;
 		}
 	}
 }
