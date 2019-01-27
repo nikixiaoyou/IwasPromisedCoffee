0ac53de70fa0e0438bcffde4048a6ece39d1e56a
jonathan_filion_ <filion.jonathan@gmail.com>
2019-01-26 22:50:58 -0500

    Stealth

--- GlobalGameJam/Assets/Scripts/Stealth/HidingSpot.cs
+++ GlobalGameJam/Assets/Scripts/Stealth/HidingSpot.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
+﻿using ggj;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HidingSpot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        
+        collider.attachedRigidbody.GetComponent<Stealth>().IsVisible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collider)
     {
-        
+        collider.attachedRigidbody.GetComponent<Stealth>().IsVisible = true;
     }
 }
